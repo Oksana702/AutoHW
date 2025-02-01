e95d04cbb8b8470550f80dfc59c7d0f171f9594b
Oksana <qasimovaoh@mail.ru>
2025-02-01 13:45:18 +0400

    Initial commit

---
+++ src/main/java/lesson14/Task01.java
@@ -0,0 +1,16 @@
+package lesson14;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task01 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.automationpractice.pl/index.php");
+
+
+    }
+}
