9b66302c50924387c9d9a1f196c7c1122faa56f9
Oksana <qasimovaoh@mail.ru>
2025-02-01 16:20:50 +0400

    add smth new

--- src/main/java/lesson14/Task02.java
+++ src/main/java/lesson14/Task02.java
@@ -4,13 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Task01 {
+public class Task02 {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.automationpractice.pl/index.php");
-
+        System.out.println("Your logo");
+        System.out.println("http://www.automationpractice.pl/index.php");
 
     }
 }
