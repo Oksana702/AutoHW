70b9125d29730cb06aa91e2242d6d686088928d6
Oksana <qasimovaoh@mail.ru>
2025-02-03 16:14:45 +0400

    add

--- src/main/java/lesson14/Task02.java
+++ src/main/java/lesson14/Task02.java
@@ -4,15 +4,52 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task02 {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://www.automationpractice.pl/index.php");
-        System.out.println("Page title: " + driver.getTitle());
-        System.out.println("Page URL: " + driver.getCurrentUrl());
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+        String[] urls = {
+                "http://www.automationpractice.pl/index.php",
+                "https://zoo.waw.pl/",
+                "https://www.w3schools.com/",
+                "https://www.clickspeedtester.com/click-counter/",
+                "https://andersenlab.com/"
+        };
 
+        for (String url : urls) {
+            driver.switchTo().newWindow(org.openqa.selenium.WindowType.TAB);
+            driver.get(url);
+            Thread.sleep(2000);
+        }
+        List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+
+        for (String tab : tabs) {
+            driver.switchTo().window(tab);
+            Thread.sleep(2000);
+
+            String title = driver.getTitle();
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("Страница: " + title + " | URL: " + currentUrl);
+
+
+        if (title.contains("Zoo")) {
+            driver.close();
+        }
+        }
+        for (String tab: driver.getWindowHandles()) {
+        driver.switchTo().window(tab);
+        break;
+        }
+
+        Thread.sleep(3000);
+        driver.quit();
+        }
     }
-}
+
+
+
+
