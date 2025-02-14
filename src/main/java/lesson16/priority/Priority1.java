dcd6805fb07c81fe4b80c7be066a2f35c635d0af
Oksana702 <qasimovaoh@mail.ru>
2025-02-14 16:47:46 +0400

    added 16

--- src/main/java/lesson16/priority/Priority1.java
+++ src/main/java/lesson16/priority/Priority1.java
@@ -1,4 +1,37 @@
 package lesson16.priority;
 
+import jdk.incubator.vector.VectorOperators;
+import org.testng.annotations.Test;
+
 public class Priority1 {
+    @Test(priority=7)
+    public void g(){
+        System.out.println("Test g");
+    }
+    @Test(priority=6)
+    public void f(){
+        System.out.println("Test f");
+    }
+    @Test(priority=5)
+    public void e(){
+        System.out.println("Test e");
+    }
+    @Test(priority=4)
+    public void d(){
+        System.out.println("Test d");
+    }
+    @Test(priority=3)
+    public void c(){
+        System.out.println("Test c");
+    }
+    @Test(priority=2)
+    public void b(){
+        System.out.println("Test b");
+    }
+    @Test(priority=1)
+    public void a(){
+        System.out.println("Test a");
+    }
+
+
 }
